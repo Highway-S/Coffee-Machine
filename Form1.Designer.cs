0edeb7e8d4e6a194c3bcee2b4beb3e8111289533
Highway-S <phoorin.w@kkumail.com>
2025-01-28 13:14:55 +0700

    ส่งงาน

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Coffee_Machine
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox cmbDrinkOptions;
+        private System.Windows.Forms.Button btnCheckStock;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Button btnRefill;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Label lblIngredients;
+        private System.Windows.Forms.Label lblDrinkInfo;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.cmbDrinkOptions = new System.Windows.Forms.ComboBox();
+            this.btnCheckStock = new System.Windows.Forms.Button();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.btnRefill = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.lblIngredients = new System.Windows.Forms.Label();
+            this.lblDrinkInfo = new System.Windows.Forms.Label();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbDrinkOptions
+            // 
+            this.cmbDrinkOptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDrinkOptions.Font = new System.Drawing.Font("Arial", 10F);
+            this.cmbDrinkOptions.FormattingEnabled = true;
+            this.cmbDrinkOptions.Location = new System.Drawing.Point(20, 30);
+            this.cmbDrinkOptions.Name = "cmbDrinkOptions";
+            this.cmbDrinkOptions.Size = new System.Drawing.Size(150, 24);
+            this.cmbDrinkOptions.TabIndex = 0;
+            // 
+            // btnCheckStock
+            // 
+            this.btnCheckStock.Location = new System.Drawing.Point(20, 110);
+            this.btnCheckStock.Name = "btnCheckStock";
+            this.btnCheckStock.Size = new System.Drawing.Size(150, 30);
+            this.btnCheckStock.TabIndex = 1;
+            this.btnCheckStock.Text = "Check Stock";
+            this.btnCheckStock.UseVisualStyleBackColor = true;
+            this.btnCheckStock.Click += new System.EventHandler(this.btnCheckStock_Click);
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Enabled = false;
+            this.btnBuy.Location = new System.Drawing.Point(20, 150);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(150, 30);
+            this.btnBuy.TabIndex = 2;
+            this.btnBuy.Text = "Buy Now";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // btnRefill
+            // 
+            this.btnRefill.Location = new System.Drawing.Point(20, 190);
+            this.btnRefill.Name = "btnRefill";
+            this.btnRefill.Size = new System.Drawing.Size(150, 30);
+            this.btnRefill.TabIndex = 3;
+            this.btnRefill.Text = "Refill Ingredients & Stock";
+            this.btnRefill.UseVisualStyleBackColor = true;
+            this.btnRefill.Click += new System.EventHandler(this.btnRefill_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(20, 230);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(180, 60);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(37, 13);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price: ";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(180, 90);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(41, 13);
+            this.lblStock.TabIndex = 6;
+            this.lblStock.Text = "Stock: ";
+            // 
+            // lblIngredients
+            // 
+            this.lblIngredients.AutoSize = true;
+            this.lblIngredients.Location = new System.Drawing.Point(180, 120);
+            this.lblIngredients.Name = "lblIngredients";
+            this.lblIngredients.Size = new System.Drawing.Size(62, 13);
+            this.lblIngredients.TabIndex = 7;
+            this.lblIngredients.Text = "Ingredients:";
+            // 
+            // lblDrinkInfo
+            // 
+            this.lblDrinkInfo.AutoSize = true;
+            this.lblDrinkInfo.Location = new System.Drawing.Point(180, 150);
+            this.lblDrinkInfo.Name = "lblDrinkInfo";
+            this.lblDrinkInfo.Size = new System.Drawing.Size(53, 13);
+            this.lblDrinkInfo.TabIndex = 8;
+            this.lblDrinkInfo.Text = "Drink Info";
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(180, 30);
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(40, 20);
+            this.numQuantity.TabIndex = 9;
+            this.numQuantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(603, 395);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.lblDrinkInfo);
+            this.Controls.Add(this.lblIngredients);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnRefill);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.btnCheckStock);
+            this.Controls.Add(this.cmbDrinkOptions);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Coffee_Machine
+{
+    public partial class Form1 : Form
+    {
+        CoffeeMachine coffeeMachine;
+
+        public Form1()
+        {
+            InitializeComponent();
+            coffeeMachine = new CoffeeMachine();
+
+            // Add options to ComboBox
+            cmbDrinkOptions.Items.Add("Black Coffee");
+            cmbDrinkOptions.Items.Add("Mocha");
+            cmbDrinkOptions.Items.Add("Latte");
+            cmbDrinkOptions.Items.Add("Chocolate");
+            cmbDrinkOptions.SelectedIndex = 0; // Default selection
+
+            // Style buttons
+            btnCheckStock.BackColor = Color.DodgerBlue;
+            btnCheckStock.ForeColor = Color.White;
+            btnCheckStock.Font = new Font("Arial", 10, FontStyle.Bold);
+            btnCheckStock.FlatStyle = FlatStyle.Flat;
+            btnCheckStock.FlatAppearance.BorderSize = 1;
+            btnCheckStock.FlatAppearance.BorderColor = Color.DarkBlue;
+
+            btnBuy.BackColor = Color.ForestGreen;
+            btnBuy.ForeColor = Color.White;
+            btnBuy.Font = new Font("Arial", 10, FontStyle.Bold);
+            btnBuy.FlatStyle = FlatStyle.Flat;
+            btnBuy.FlatAppearance.BorderSize = 1;
+            btnBuy.FlatAppearance.BorderColor = Color.DarkGreen;
+
+            UpdateDrinkInfo();
+        }
+
+        // เมื่อคลิกปุ่มเช็คสต็อก
+        private void btnCheckStock_Click(object sender, EventArgs e)
+        {
+            string selectedDrink = cmbDrinkOptions.SelectedItem.ToString();
+            int quantity = (int)numQuantity.Value;  // Get the quantity selected by user
+
+            bool isStockAvailable = coffeeMachine.CheckStock(selectedDrink, quantity);  // เช็คสต็อกตามจำนวนที่เลือก
+            bool isIngredientsEnough = coffeeMachine.CheckIngredients(selectedDrink);
+
+            if (isStockAvailable && isIngredientsEnough)
+            {
+                lblStatus.Text = $"You can buy {quantity} {selectedDrink}(s).";
+                btnBuy.Enabled = true; // Enable "Buy" button only if stock and ingredients are enough
+            }
+            else if (!isStockAvailable)
+            {
+                lblStatus.Text = $"Not enough stock for {selectedDrink}. Please restock.";
+                btnBuy.Enabled = false;
+            }
+            else if (!isIngredientsEnough)
+            {
+                lblStatus.Text = $"Not enough ingredients for {selectedDrink}. Please refill.";
+                btnBuy.Enabled = false;
+            }
+        }
+
+        // เมื่อคลิกปุ่มซื้อสินค้า
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            string selectedDrink = cmbDrinkOptions.SelectedItem.ToString();
+            int quantity = (int)numQuantity.Value;
+
+            // ลบสต็อกของเครื่องดื่มที่เลือกจำนวนที่ซื้อลง
+            for (int i = 0; i < quantity; i++)
+            {
+                coffeeMachine.MakeDrink(selectedDrink);
+            }
+
+            lblStatus.Text = $"Your {quantity} {selectedDrink}(s) are ready!";
+            btnBuy.Enabled = false;  // Disable "Buy" button after purchase
+            UpdateDrinkInfo();  // อัพเดทข้อมูลสต็อกหลังการซื้อ
+        }
+
+        // อัพเดทข้อมูลแสดงผล
+        private void UpdateDrinkInfo()
+        {
+            var selectedDrink = cmbDrinkOptions.SelectedItem.ToString();
+            if (coffeeMachine.drinks.ContainsKey(selectedDrink))
+            {
+                var drink = coffeeMachine.drinks[selectedDrink];
+                lblDrinkInfo.Text = $"Price: {drink.Price} THB\nStock: {drink.Stock}\nIngredients:\n Water={drink.Water}g\n Coffee={drink.Coffee}g\n Milk={drink.Milk}g\n Chocolate={drink.Chocolate}g";
+                lblPrice.Text = $"Price: {drink.Price} THB";
+                lblStock.Text = $"Stock: {drink.Stock}";
+                lblIngredients.Text = $"Ingredients: Water={drink.Water}g, Coffee={drink.Coffee}g, Milk={drink.Milk}g, Chocolate={drink.Chocolate}g";
+            }
+        }
+
+        // เมื่อคลิกปุ่มรีฟิล
+        private void btnRefill_Click(object sender, EventArgs e)
+        {
+            coffeeMachine.RefillIngredients();
+            coffeeMachine.RefillStock();
+            lblStatus.Text = "Ingredients and stock refilled successfully.";
+            UpdateDrinkInfo();
+        }
+    }
+
+    // Class สำหรับจัดการเครื่องดื่มต่างๆ
+    public class CoffeeMachine
+    {
+        private int Water;
+        private int Coffee;
+        private int Milk;
+        private int Chocolate;
+        public Dictionary<string, Drink> drinks;
+
+        public CoffeeMachine()
+        {
+            Water = 1000;
+            Coffee = 500;
+            Milk = 200;
+            Chocolate = 200;
+
+            drinks = new Dictionary<string, Drink>
+            {
+                {"Black Coffee", new Drink("Black Coffee", 300, 20, 0, 0, 10, 50)},
+                {"Mocha", new Drink("Mocha", 300, 20, 0, 10, 10, 60)},
+                {"Latte", new Drink("Latte", 300, 20, 10, 0, 10, 55)},
+                {"Chocolate", new Drink("Chocolate", 300, 0, 0, 20, 10, 45)}
+            };
+        }
+
+        // เช็คส่วนผสมเครื่องดื่ม
+        public bool CheckIngredients(string drinkName)
+        {
+            if (drinks.ContainsKey(drinkName))
+            {
+                var drink = drinks[drinkName];
+                return Water >= drink.Water && Coffee >= drink.Coffee && Milk >= drink.Milk && Chocolate >= drink.Chocolate;
+            }
+            return false;
+        }
+
+        // เช็คสต็อกเครื่องดื่มตามจำนวนที่ต้องการซื้อ
+        public bool CheckStock(string drinkName, int quantity)
+        {
+            if (drinks.ContainsKey(drinkName))
+            {
+                return drinks[drinkName].Stock >= quantity;  // เช็คว่าในสต็อกมีเพียงพอหรือไม่
+            }
+            return false;
+        }
+
+        // สร้างเครื่องดื่ม
+        public void MakeDrink(string drinkName)
+        {
+            if (drinks.ContainsKey(drinkName))
+            {
+                var drink = drinks[drinkName];
+
+                Water -= drink.Water;
+                Coffee -= drink.Coffee;
+                Milk -= drink.Milk;
+                Chocolate -= drink.Chocolate;
+
+                drink.Stock -= 1;  // ลดสต็อกของเครื่องดื่มที่เลือก
+            }
+        }
+
+        // รีฟิลส่วนผสมเครื่องดื่ม
+        public void RefillIngredients()
+        {
+            Water = 100000;
+            Coffee = 50000;
+            Milk = 20000;
+            Chocolate = 20000;
+        }
+
+        // รีฟิลสต็อกเครื่องดื่มแต่ละชนิด
+        public void RefillStock()
+        {
+            foreach (var drink in drinks.Values)
+            {
+                drink.Stock = 10;  // รีฟิลจำนวนสต็อกที่ 10 สำหรับแต่ละชนิด
+            }
+        }
+    }
+
+    // Class สำหรับเครื่องดื่มแต่ละชนิด
+    public class Drink
+    {
+        public string Name { get; set; }
+        public int Water { get; set; }
+        public int Coffee { get; set; }
+        public int Milk { get; set; }
+        public int Chocolate { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+
+        public Drink(string name, int water, int coffee, int milk, int chocolate, int stock, int price)
+        {
+            Name = name;
+            Water = water;
+            Coffee = coffee;
+            Milk = milk;
+            Chocolate = chocolate;
+            Stock = stock;
+            Price = price;
+        }
+    }
+}
